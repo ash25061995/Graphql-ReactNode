f0bfd406a970ea724efa2476988d5d10ab88a6b2
Ashwani <ashwani250695@gmail.com>
2020-07-14 00:28:08 +0530

    adding client

---
+++ client/src/components/BookList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BookList = () => {
+    return (
+        <div>
+            <h1>Book List</h1>
+            <ul id="book-list">
+                <li>Book name</li>
+            </ul>
+        </div>
+    );
+};
+
+export default BookList;
